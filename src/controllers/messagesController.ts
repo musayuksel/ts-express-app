c72dd0f98b6e096b6d7684423f864647d1b76f46
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-16 17:06:38 +0100

    Add createMessage

--- src/controllers/messagesController.ts
+++ src/controllers/messagesController.ts
@@ -13,3 +13,17 @@ export const getMessageByUserId = (req: Request, res: Response) => {
 
   res.json(userMessages);
 };
+
+export const createMessage = (req: Request, res: Response) => {
+  const { content, user_id, time_stamp, attachment } = req.body;
+  const newId = +messages.messages.length + 1 + "";
+  const newMessage = {
+    id: newId,
+    content,
+    user_id,
+    time_stamp,
+    attachment: attachment || null,
+  };
+  messages.messages.push(newMessage);
+  res.json(newMessage);
+};
