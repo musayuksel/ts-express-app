9c749d52889fcfc4f30463cb1410d23649256347
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-16 19:13:48 +0100

    Add error handler for routes

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ app.use(express.json());
 
 app.use("/api/health-check", healthCheckRoutes);
 app.use("/api/messages", messagesRoutes);
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Route not found",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
