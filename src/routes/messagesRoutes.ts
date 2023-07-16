f6d50168bebc4879afd6cbfc5f581252bbf5ede2
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-16 17:07:47 +0100

    Add middleware chaining

--- src/routes/messagesRoutes.ts
+++ src/routes/messagesRoutes.ts
@@ -2,11 +2,15 @@ import express, { Response } from "express";
 import {
   getAllMessages,
   getMessageByUserId,
+  createMessage,
 } from "../controllers/messagesController";
+
+import { logBody } from "../middlewares/messageMiddleware";
 
 const router = express.Router();
 
 router.get("/", getAllMessages);
 router.get("/:userId", getMessageByUserId);
+router.post("/", logBody, createMessage);
 
 export default router;
