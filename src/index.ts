c5d8bd0e75ac48239d3270bc5692e55151f010b6
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-17 17:22:31 +0100

    Connect db

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,11 @@ import {
   customError,
   globalErrorHandler,
 } from "./middlewares/globalErrorMiddleware";
+import { Sequelize } from "sequelize";
 
 dotenv.config();
 const PORT = parseInt(process.env.PORT || "3000");
+const DB_URL = process.env.DB_URL || "postgres://localhost:5432/slack";
 
 const app = express();
 app.use(express.json());
@@ -17,14 +19,24 @@ app.use("/api/health-check", healthCheckRoutes);
 app.use("/api/messages", messagesRoutes);
 
 app.use("*", (req: Request, res: Response, next: NextFunction) => {
-  // res.status(404).json({
-  //   status: "fail",
-  //   message: "Route not found",
-  // });
   const error = new customError(`Route ${req.originalUrl} not found!!!`, 404);
   next(error);
 });
+
 app.use(globalErrorHandler);
+
+const connectDB = async () => {
+  const sequelize = new Sequelize(DB_URL);
+  try {
+    await sequelize.authenticate();
+    console.log(
+      `Connected to ${sequelize.getDatabaseName()} postgres database`
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
