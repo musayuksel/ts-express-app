fcb8a5220c9b2be7c7e380ca50cd7ee43bf0b1d1
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-22 14:35:38 +0100

    Add user Model

---
+++ src/models/user.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "./sequelize";
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    userEmail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+  },
+  { sequelize }
+);
+
+export { User };
