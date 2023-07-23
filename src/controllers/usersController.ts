7335c99e7e574ebcc09f308d94364f08a1d9cb61
Musa <79163385+musayuksel@users.noreply.github.com>
2023-07-23 19:42:21 +0100

    Add new user validations

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,8 +1,24 @@
 import { Request, Response } from "express";
 import { User } from "../models/user";
+import { customError } from "../middlewares/globalErrorMiddleware";
 
 export const createNewUser = async (req: Request, res: Response) => {
   const { userName, userEmail, firstName, lastName } = req.body;
+
+  //   TODO: validate the input data with an efficient way
+  if (!userName || !userEmail || !firstName || !lastName) {
+    throw new customError("Please provide all the details", 400);
+  }
+  // check if user already exists
+  const user = await User.findOne({
+    where: {
+      userEmail,
+    },
+  });
+  if (user) {
+    throw new customError("User already exists", 400);
+  }
+
   const user1 = await User.create({
     userName,
     userEmail,
