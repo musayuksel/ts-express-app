322ef42ac9d04cae39cd58f3cb91ca64bd9cd9a9
Robert Wilkinson <robert.wilkinson@sky.uk>
2023-07-24 13:51:37 +0100

    update env var to user DB_HOST var

--- custom.d.ts
+++ custom.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test'
     DB_HOST: string;
+    DB_DB: string;
     DB_USERNAME: string;
     DB_PASSWORD: string;
   }
--- src/models/sequelize.ts
+++ src/models/sequelize.ts
@@ -2,12 +2,17 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const DB_DB = process.env.DB_DB || 'slack';
+const DB_DB = process.env.DB_DB;
+const DB_HOST = process.env.DB_HOST;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
+if (!DB_DB || !DB_HOST || !DB_USERNAME || !DB_PASSWORD) {
+  throw new Error('Missing required environment variables for database connection.');
+}
+
 const sequelize = new Sequelize(DB_DB, DB_USERNAME, DB_PASSWORD, {
-  host: 'localhost',
+  host: DB_HOST,
   dialect: 'postgres',
 });
 
