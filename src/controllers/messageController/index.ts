965f067f0817136087937b11e05e0ce03740806c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-03 22:29:09 +0100

    Delete message from DB and S3

--- src/controllers/messageController/index.ts
+++ src/controllers/messageController/index.ts
@@ -2,12 +2,14 @@ import { getAllMessages } from './getAllMessages';
 import { createMessage } from './createMessage';
 import { getUserMessages } from './getUserMessages';
 import { uploadFile } from './uploadFile';
+import { deleteMessage } from './deleteMessage';
 
 const messageController = {
   getAllMessages,
   getUserMessages,
   createMessage,
   uploadFile,
+  deleteMessage,
 };
 
 export default messageController;
