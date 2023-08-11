cff867cb89e7e23d04bc76b37a2821cf04d58817
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-11 11:50:58 +0100

    console the current user

--- src/middlewares/messageMiddleware.ts
+++ src/middlewares/messageMiddleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const logBody = (req: Request, res: Response, next: NextFunction) => {
+  console.log({ currentUser: req.currentUser });
   console.log({ RequestBody: req.body });
   next();
 };
