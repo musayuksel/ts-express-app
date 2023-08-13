1f6df58a947d560f4742cc7709b693feef4b3985
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-13 22:26:18 +0100

    Refactor:lint err

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ async function startServer() {
   }
 
   app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.info(`Server is running on port ${PORT}`);
   });
 }
 
