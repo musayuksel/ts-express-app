34af9b445dfba84e332d22912843d07e4d89ac52
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-14 21:43:44 +0100

    Add getUserChannels controller

--- src/controllers/channelController/index.ts
+++ src/controllers/channelController/index.ts
@@ -1,11 +1,13 @@
 import { getAllChannels } from './getAllChannels';
 import { createChannel } from './createChannel';
 import { addUserToChannel } from './addUserToChannel';
+import { getUserChannels } from './getUserChannels';
 
 const channelController = {
   getAllChannels,
   createChannel,
   addUserToChannel,
+  getUserChannels,
 };
 
 export default channelController;
