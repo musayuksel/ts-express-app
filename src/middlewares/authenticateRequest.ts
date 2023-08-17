c0763f4f227ee8b55f0470f36053bd72830e58de
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-17 14:50:54 +0100

    Use env vars

--- src/middlewares/authenticateRequest.ts
+++ src/middlewares/authenticateRequest.ts
@@ -3,17 +3,19 @@ import { CognitoJwtVerifier } from 'aws-jwt-verify';
 import { CustomError } from './globalErrorHandler';
 
 export const authenticateRequest = async (req: Request, res: Response, next: NextFunction) => {
-  //   const verifier = CognitoJwtVerifier.create({
-  //     region: process.env.AWS_REGION || 'eu-west-2',
-  //     tokenUse: 'access',
-  //     userPoolId: process.env.AWS_COGNITO_USER_POOL_ID || 'eu-west-2_V',
-  //     clientId: process.env.AWS_COGNITO_CLIENT_ID,
-  //   });
+  const AWS_REGION = process.env.AWS_REGION;
+  const AWS_COGNITO_USER_POOL_ID = process.env.AWS_COGNITO_USER_POOL_ID;
+  const AWS_COGNITO_USER_POOL_CLIENT_ID = process.env.AWS_COGNITO_USER_POOL_CLIENT_ID;
+
+  if (!AWS_REGION || !AWS_COGNITO_USER_POOL_ID || !AWS_COGNITO_USER_POOL_CLIENT_ID) {
+    throw new Error('Missing AWS Cognito config');
+  }
+
   const verifier = CognitoJwtVerifier.create({
-    region: 'eu-west-2',
+    region: AWS_REGION,
     tokenUse: 'access',
-    userPoolId: 'eu-west-2_VUdj4DoBW',
-    clientId: '229j8tmhe7ro5s6dtgcj2fa73e',
+    userPoolId: AWS_COGNITO_USER_POOL_ID,
+    clientId: AWS_COGNITO_USER_POOL_CLIENT_ID,
   });
 
   const accessToken = req.headers.authorization?.split(' ')[1] || '';
