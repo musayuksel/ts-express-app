c5bb5e51044ae2e6f39efdb6f0cccf897797cb4b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-21 13:33:31 +0100

    Mock channels and functions

--- src/controllers/channelController/getAllChannels/getAllChannels.test.ts
+++ src/controllers/channelController/getAllChannels/getAllChannels.test.ts
@@ -1,5 +1,42 @@
+import { Request, Response, NextFunction } from 'express';
+import { Channel } from '../../../models/channel';
+import { getAllChannels } from './getAllChannels';
+
+jest.mock('../../../models/channel', () => ({
+  Channel: {
+    findAll: jest.fn(),
+  },
+}));
+
+const req = {} as Request;
+const res = {
+  json: jest.fn(),
+} as unknown as Response;
+const next = jest.fn() as NextFunction;
+
+const mockChannels = [
+  {
+    id: 1,
+    channelName: 'channel 1',
+    createdAt: '2023-08-14T20:17:39.681Z',
+    updatedAt: '2023-08-14T20:17:39.681Z',
+  },
+  {
+    id: 2,
+    channelName: 'channel 2',
+    createdAt: '2023-08-14T20:17:39.681Z',
+    updatedAt: '2023-08-14T20:17:39.681Z',
+  },
+];
+
 describe('getAllChannels', () => {
-  it('should return all channels', async () => {
-    expect(true).toBe(true);
+  it('should retrieve all channels and send them', async () => {
+    (Channel.findAll as jest.Mock).mockResolvedValue(mockChannels);
+
+    await getAllChannels(req, res, next);
+
+    expect(Channel.findAll).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith(mockChannels);
+    expect(next).not.toHaveBeenCalled();
   });
 });
