f881125470e9c103bab3c76ebf7f1e00b4163b36
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-27 21:41:39 +0100

    add userToChannelSchema

--- src/routes/channelsRoutes.ts
+++ src/routes/channelsRoutes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { channelController } from '../controllers';
 import { validateReqBodySchema } from '../middlewares/validateReqBodySchema';
 import { channelSchema } from '../schemas';
+import { addUserToChannelSchema } from '../schemas/channel.schema';
 
 const router = Router();
 
 router.get('/', channelController.getAllChannels);
 router.get('/userChannels', channelController.getUserChannels);
 router.post('/', validateReqBodySchema(channelSchema), channelController.createChannel);
-router.post('/addUser', channelController.addUserToChannel);
+router.post('/addUser', validateReqBodySchema(addUserToChannelSchema), channelController.addUserToChannel);
 
 export default router;
--- src/schemas/channel.schema.ts
+++ src/schemas/channel.schema.ts
@@ -3,3 +3,8 @@ import * as Joi from 'joi';
 export const channelSchema = Joi.object({
   channelName: Joi.string().required(),
 });
+
+export const addUserToChannelSchema = Joi.object({
+  userId: Joi.string().required(),
+  channelId: Joi.string().required(),
+});
