dc86a949f4804d9a793a4fd9907f99d05376296d
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-28 20:12:24 +0100

    Delete comments

--- src/controllers/messageController/operations/createMessageOperation/createMessageOperation.test.ts
+++ src/controllers/messageController/operations/createMessageOperation/createMessageOperation.test.ts
@@ -36,30 +36,3 @@ describe('createMessageOperation', () => {
     );
   });
 });
-
-// export const createMessageOperation = async (messagePayload: CreateMessageOperationTypes) => {
-//     const { userId, channelId } = messagePayload;
-
-//     const currentChannel = await prismaClient.channels.findUnique({
-//       where: {
-//         id: channelId,
-//       },
-//       include: {
-//         users: true,
-//       },
-//     });
-
-//     if (!currentChannel) {
-//       throw new CustomError(`Channel does not exist!`, 404);
-//     }
-
-//     const isUserInChannel = currentChannel.users.find((user) => user.id === userId);
-
-//     if (!isUserInChannel) {
-//       throw new CustomError(`User is not in the channel! Please join the channel first!`, 404);
-//     }
-
-//     return await prismaClient.messages.create({
-//       data: messagePayload,
-//     });
-//   };
