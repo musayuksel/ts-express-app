507936ab4bde241837d1e06292dee0610527ea30
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-28 12:08:21 +0100

    Add error coverage test

--- src/controllers/channelController/operations/createChannelOperation/createChannelOperation.test.ts
+++ src/controllers/channelController/operations/createChannelOperation/createChannelOperation.test.ts
@@ -2,16 +2,28 @@ import { prismaMock } from '../../../../lib/prisma';
 import { createChannelOperation } from './createChannelOperation';
 
 describe('create operation', () => {
+  const mockDate = new Date('2021-01-01T00:00:00.000Z');
+
+  const mockChannel = {
+    id: 'uuid_random_string',
+    channelName: 'testChannelName',
+    createdAt: mockDate,
+    updatedAt: mockDate,
+  };
+
   it('should create the channel', async () => {
-    prismaMock.channels.create.mockResolvedValue({
-      id: 'uuid_random_string',
-      channelName: 'testChannelName',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    });
+    prismaMock.channels.create.mockResolvedValue(mockChannel);
 
     const createdChannel = await createChannelOperation({ channelName: 'testChannelName' });
 
-    expect(createdChannel?.channelName).toBe('testChannelName');
+    expect(createdChannel.channelName).toBe('testChannelName');
+  });
+
+  it('should throw an error if channel already exists', async () => {
+    prismaMock.channels.findUnique.mockResolvedValue(mockChannel);
+
+    await expect(() => createChannelOperation({ channelName: 'testChannelName' })).rejects.toThrow(
+      'Channel with name "testChannelName" already exists',
+    );
   });
 });
