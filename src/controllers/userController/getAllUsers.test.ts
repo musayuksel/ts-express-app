831dd938dc1ad18b0f37357f7aad039d8049ba4f
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-30 22:51:37 +0100

    add getAllUsers test

---
+++ src/controllers/userController/getAllUsers.test.ts
@@ -0,0 +1,31 @@
+import { mockCognitoJwtVerifier, mockFunction } from '../utils';
+import { getAllUsersOperation } from './operations';
+import { formatResponse } from '../../utils';
+import request from 'supertest';
+import { app } from '../../app';
+
+jest.mock('./operations', () => ({
+  getAllUsersOperation: mockFunction,
+}));
+
+jest.mock('../../utils', () => ({
+  formatResponse: mockFunction,
+}));
+
+jest.mock('aws-jwt-verify', () => ({
+  ...jest.requireActual('aws-jwt-verify'),
+  CognitoJwtVerifier: mockCognitoJwtVerifier,
+}));
+
+describe('getAllUsers', () => {
+  it('should return a 200 response code and all users', async () => {
+    const response = await request(app).get('/api/users').set('Authorization', 'Bearer mockToken');
+
+    expect(getAllUsersOperation).toHaveBeenCalledWith();
+
+    expect(formatResponse).toHaveBeenCalledWith({
+      success: true,
+      data: response.body.data,
+    });
+  });
+});
