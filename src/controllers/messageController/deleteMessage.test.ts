22352c690969bcc3ec42509de27bc96468e990d1
Musa <79163385+musayuksel@users.noreply.github.com>
2023-08-30 22:25:20 +0100

    add deleteMessage test

---
+++ src/controllers/messageController/deleteMessage.test.ts
@@ -0,0 +1,31 @@
+import { mockCognitoJwtVerifier, mockFunction } from '../utils';
+import { deleteMessageOperation } from './operations';
+import { formatResponse } from '../../utils';
+import request from 'supertest';
+import { app } from '../../app';
+
+jest.mock('./operations', () => ({
+  deleteMessageOperation: mockFunction,
+}));
+
+jest.mock('../../utils', () => ({
+  formatResponse: mockFunction,
+}));
+
+jest.mock('aws-jwt-verify', () => ({
+  ...jest.requireActual('aws-jwt-verify'),
+  CognitoJwtVerifier: mockCognitoJwtVerifier,
+}));
+
+describe('deleteMessage', () => {
+  it('should return a 200 response code and deleted message', async () => {
+    const response = await request(app).delete('/api/messages/mockMessageId').set('Authorization', 'Bearer mockToken');
+
+    expect(deleteMessageOperation).toHaveBeenCalledWith({ messageId: 'mockMessageId' });
+
+    expect(formatResponse).toHaveBeenCalledWith({
+      success: true,
+      data: response.body.data,
+    });
+  });
+});
