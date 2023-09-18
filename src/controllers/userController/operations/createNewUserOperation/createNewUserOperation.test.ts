0af83e9b3dea4fb3d60cc5fa7bfadd7cd4c362e4
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-18 22:04:04 +0100

    Update createNewUser operations and tests

--- src/controllers/userController/operations/createNewUserOperation/createNewUserOperation.test.ts
+++ src/controllers/userController/operations/createNewUserOperation/createNewUserOperation.test.ts
@@ -1,20 +1,28 @@
-import { mockUser, prismaMock } from '../../../../lib';
+import { Context, MockContext, createMockContext, mockUser } from '../../../../lib';
 import { createNewUserOperation } from './createNewUserOperation';
+
+let mockContext: MockContext;
+let context: Context;
+
+beforeEach(() => {
+  mockContext = createMockContext();
+  context = mockContext as unknown as Context;
+});
 
 describe('createNewUserOperation', () => {
   it('should create a new user', async () => {
-    prismaMock.users.findUnique.mockResolvedValue(null);
-    prismaMock.users.create.mockResolvedValue(mockUser);
+    mockContext.prismaClient.users.findUnique.mockResolvedValue(null);
+    mockContext.prismaClient.users.create.mockResolvedValue(mockUser);
 
-    const createdUser = await createNewUserOperation(mockUser);
+    const createdUser = await createNewUserOperation(mockUser, context);
 
     expect(createdUser).toBe(mockUser);
   });
 
   it('should throw an error if user already exists', async () => {
-    prismaMock.users.findUnique.mockResolvedValue(mockUser);
+    mockContext.prismaClient.users.findUnique.mockResolvedValue(mockUser);
 
-    await expect(() => createNewUserOperation(mockUser)).rejects.toThrow(
+    await expect(() => createNewUserOperation(mockUser, context)).rejects.toThrow(
       `User with name "${mockUser.userName}" already exists`,
     );
   });
