a4aac3813d1d1b8c0b2c1e51350a873b77307e8f
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-18 21:51:05 +0100

    Update getUserMessages operations and tests

--- src/controllers/messageController/operations/getUserMessagesOperation/getUserMessagesOperation.test.ts
+++ src/controllers/messageController/operations/getUserMessagesOperation/getUserMessagesOperation.test.ts
@@ -1,16 +1,24 @@
-import { mockMessage, mockUser, prismaMock } from '../../../../lib';
+import { Context, MockContext, createMockContext, mockMessage, mockUser, prismaMock } from '../../../../lib';
 import { getUserMessagesOperation } from './getUserMessagesOperation';
 
 jest.mock('../../utils', () => ({
   addSignedUrlToMessage: jest.fn().mockImplementation((message) => message),
 }));
 
+let mockContext: MockContext;
+let context: Context;
+
+beforeEach(() => {
+  mockContext = createMockContext();
+  context = mockContext as unknown as Context;
+});
+
 describe('getUserMessagesOperation', () => {
   it('should return all messages for a user', async () => {
     const mockUserWithMessages = { ...mockUser, messages: [{ ...mockMessage }] };
-    prismaMock.users.findUnique.mockResolvedValue(mockUserWithMessages);
+    mockContext.prismaClient.users.findUnique.mockResolvedValue(mockUserWithMessages);
 
-    const messages = await getUserMessagesOperation({ userId: 'testUserId' });
+    const messages = await getUserMessagesOperation({ userId: 'testUserId' }, context);
 
     expect(messages).toHaveLength(1);
     expect(messages[0].content).toBe(mockMessage.content);
@@ -19,6 +27,6 @@ describe('getUserMessagesOperation', () => {
   it('should throw an error if user does not exist', async () => {
     prismaMock.users.findUnique.mockResolvedValue(null);
 
-    await expect(() => getUserMessagesOperation({ userId: 'testUserId' })).rejects.toThrow('User not found');
+    await expect(() => getUserMessagesOperation({ userId: 'testUserId' }, context)).rejects.toThrow('User not found');
   });
 });
