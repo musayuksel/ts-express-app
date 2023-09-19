e729728fcb43ea7f68f978b11b250ebd3fa4c141
Musa <79163385+musayuksel@users.noreply.github.com>
2023-09-19 13:48:50 +0100

    Add notes for mock test

--- src/controllers/channelController/getUserChannels/getUserChannels.test.ts
+++ src/controllers/channelController/getUserChannels/getUserChannels.test.ts
@@ -1,8 +1,8 @@
 import { mockCognitoJwtVerifier, mockFunction } from '../../utils';
-// import request from 'supertest';
-// import { app } from '../../app';
-// import { getUserChannelsOperation } from './operations';
-// import { formatResponse } from '../../utils';
+// import request, { Request } from 'supertest';
+// import { app } from '../../../app';
+// import { getUserChannelsOperation } from '../operations';
+// import { formatResponse } from '../../../utils';
 
 jest.mock('../operations', () => ({
   getUserChannelsOperation: mockFunction,
@@ -17,14 +17,49 @@ jest.mock('aws-jwt-verify', () => ({
   CognitoJwtVerifier: mockCognitoJwtVerifier,
 }));
 
-describe('getUserChannels', () => {
-  it('should return a 200 response code and user channels', async () => {
-    // const response = await request(app).get('api/channels/userChannels').set('Authorization', 'Bearer mockToken');
+jest.mock('../../../lib', () => ({
+  ...jest.requireActual('../../../lib'),
+  prismaClient: 'mockPrismaClient',
+}));
 
+// interface CustomRequest extends Request {
+//   currentUser: {
+//     username: string;
+//   };
+// }
+
+describe('getUserChannels', () => {
+  it('should return true', () => {
     expect(true).toBe(true);
-    // https://stackoverflow.com/questions/26794541/access-to-req-object-in-supertest-after-a-response
-    // expect(getUserChannelsOperation).toHaveBeenCalledWith({
-    //   userName: 'mockUsername',
-    // });
   });
+
+  // it('should return a 200 response code and user channels', async () => {
+  //   // Mock user payload
+  //   const userPayload = {
+  //     username: 'mockUsername',
+  //   };
+
+  //   // Mock req object with currentUser
+  //   const req: CustomRequest = {
+  //     currentUser: userPayload,
+  //   } as CustomRequest;
+  //   // Make the request using the mocked req object
+  //   const response = await request(app)
+  //     .get('/api/channels/userChannels')
+  //     .set('Authorization', 'Bearer mockToken')
+  //     .send()
+  //     .expect(200);
+
+  //   expect(getUserChannelsOperation).toHaveBeenCalledWith(
+  //     { username: userPayload.username },
+  //     { prismaClient: 'mockPrismaClient' },
+  //   );
+
+  //   expect(response.statusCode).toBe(200);
+
+  //   expect(formatResponse).toHaveBeenCalledWith({
+  //     success: true,
+  //     data: response.body.data,
+  //   });
+  // });
 });
